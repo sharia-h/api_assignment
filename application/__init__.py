db5018f671ff070df7086c415d202483806b68a9
sharia.h <sharia.hasan@symbtechnologies.com>
2023-02-23 23:18:15 +0530

    Code Check in

--- application/__init__.py
+++ application/__init__.py
@@ -3,5 +3,7 @@ def create_app():
     app=Flask(__name__)
     with app.app_context():
         from application.routes import country
+        from application.errors import handlers
+        app.register_blueprint(handlers.errors)
         app.register_blueprint(country.country_bp)
         return app
--- application/routes/country.py
+++ application/routes/country.py
@@ -2,4 +2,4 @@ from flask import Blueprint, Response
 country_bp=Blueprint("country_bp", __name__)
 @country_bp.route("/country", methods=["GET"])
 def country():
-    return(Response ("Hello"))
+    return(Response ("Hello world"))
